d8c4f3efb6022e504324333d7780057df6283e18
Sandro Caceres <sandro.caceres.olave@gmail.com>
2024-07-02 15:59:46 -0500

    feat(Product): All product models, endpoints and tests

--- drfecommerce/settings/base.py
+++ drfecommerce/settings/base.py
@@ -43,7 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # External Packages
     'rest_framework',
+    "drf_spectacular",
     # Internal Apps
+    "drfecommerce.product",
 ]
 
 MIDDLEWARE = [
@@ -120,4 +122,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-REST_FRAMEWORK = {}
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS' : 'drf_spectacular.openapi.AutoSchema'
+}
+
+SPECTACULAR_SETTUINGS = {
+    "TITLE": "Django DRF Ecommerce"
+}
