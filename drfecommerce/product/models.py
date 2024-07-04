42ed2b2caca8686f8406a1a92830de6db0e87b6c
Sandro Caceres <sandro.caceres.olave@gmail.com>
2024-07-04 16:16:39 -0500

    serializers modified

--- drfecommerce/product/models.py
+++ drfecommerce/product/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
 
-# Create your models here.
+
 class Category(MPTTModel):
     name = models.CharField(max_length=100, unique=True)
-    parent = TreeForeignKey("self", on_delete=models.PROTECT, null=True, blank=True)
+    parent = TreeForeignKey(
+        "self", on_delete=models.PROTECT, null=True, blank=True
+    )
 
     class MTTPMeta:
         order_insertion_by = ["name"]
@@ -12,18 +14,34 @@ class Category(MPTTModel):
     def __str__(self) -> str:
         return self.name
 
+
 class Brand(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
     def __str__(self) -> str:
         return self.name
 
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=255)
     description = models.TextField(blank=True)
     is_digital = models.BooleanField(default=False)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
-    category = TreeForeignKey('Category', on_delete=models.SET_NULL,null=True, blank=True)
+    category = TreeForeignKey(
+        "Category", on_delete=models.SET_NULL, null=True, blank=True
+    )
+    is_active = models.BooleanField(default=False)
 
     def __str__(self) -> str:
         return self.name
+
+
+class ProductLine(models.Model):
+    price = models.DecimalField(decimal_places=2, max_digits=5)
+    sku = models.CharField(max_length=100)
+    stock_qty = models.IntegerField()
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="product_line"
+    )
+    is_active = models.BooleanField(default=False)
--- drfecommerce/product/views.py
+++ drfecommerce/product/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render
 from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from .models import Category, Brand, Product
 from .serializers import CategorySerializer, BrandSerializer, ProductSerializer
 
+
 class CategoryViewSet(viewsets.ViewSet):
     """
-        A simple Viewset for viewing all categories
+    A simple Viewset for viewing all categories
     """
 
     queryset = Category.objects.all()
@@ -16,11 +17,12 @@ class CategoryViewSet(viewsets.ViewSet):
     @extend_schema(responses=CategorySerializer)
     def list(self, request):
         serializer = CategorySerializer(self.queryset, many=True)
-        return Response(serializer.data)    
+        return Response(serializer.data)
+
 
 class BrandViewSet(viewsets.ViewSet):
     """
-        A simple Viewset for viewing all brands
+    A simple Viewset for viewing all brands
     """
 
     queryset = Brand.objects.all()
@@ -30,16 +32,36 @@ class BrandViewSet(viewsets.ViewSet):
         serializer = BrandSerializer(self.queryset, many=True)
         return Response(serializer.data)
 
+
 class ProductViewSet(viewsets.ViewSet):
     """
-        A simple Viewset for viewing all Products
+    A simple Viewset for viewing all Products
     """
 
     queryset = Product.objects.all()
+    lookup_field = "slug"
+
+    def retrieve(self, request, slug=None):
+        serializer = ProductSerializer(
+            self.queryset.filter(slug=slug), many=True
+        )
+        return Response(serializer.data)
 
     @extend_schema(responses=ProductSerializer)
     def list(self, request):
         serializer = ProductSerializer(self.queryset, many=True)
         return Response(serializer.data)
 
-
+    @action(
+        methods=["get"],
+        detail=False,
+        url_path=r"category/(?P<category>\w+)/all",
+    )
+    def list_product_by_category(self, request, category=None):
+        """
+        An endpoint to return products by category
+        """
+        serializer = ProductSerializer(
+            self.queryset.filter(category__name=category), many=True
+        )
+        return Response(serializer.data)
